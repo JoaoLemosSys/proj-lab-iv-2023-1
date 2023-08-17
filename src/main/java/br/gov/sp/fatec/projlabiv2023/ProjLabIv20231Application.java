128fe592cfe9f0712c9b642d2b2d8348fbc3f1a5
João Lemos <joao.lemos.sys@gmail.com>
2023-08-16 22:16:14 -0300

    Initial Commit

---
+++ src/main/java/br/gov/sp/fatec/projlabiv2023/ProjLabIv20231Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projlabiv2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjLabIv20231Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjLabIv20231Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/projlabiv2023/ProjLabIv20231ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projlabiv2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjLabIv20231ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
